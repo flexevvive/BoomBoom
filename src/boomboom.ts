f9eac452509002196076dad0fe6011bdd985a46c
Evvive <flexevvive@gmail.com>
2022-02-26 00:09:57 +0100

    Better method handling

--- src/boomboom.ts
+++ src/boomboom.ts
@@ -2,6 +2,7 @@ import { Types } from "./types";
 import { Stack } from "./stack";
 import { VStack, VariableMap } from "./vstack";
 import { Logger } from "./logger";
+import { IOBuffer } from "./iobuffer";
 
 export interface BBMethodMap {
     [name: string]: Array<any>;
@@ -55,7 +56,33 @@ export class BoomBoom {
                 continue;
             }
 
-            if (!/\s/g.test(instr) && /[a-zA-Z0-9z]/.test(instr)) {
+            if (instr.startsWith(".")) {
+                let isConst: boolean, name: string;
+
+                this.logger.debug(`(${method}) Given expression is a pop-expression`);
+
+                if (!(instr.split(".")[1].startsWith("@")))
+                    throw new BBException(`Malformed pop-expression (${instr}) in ${method}`);
+
+                if (instr.split(".")[1].split("@")[1].endsWith("#")) {
+                    isConst = true;
+                    name = instr.split(".")[1].split("@")[1].split("#")[0];
+                    this.logger.debug(`(${method}) Popping into constant`);
+                } else {
+                    isConst = false;
+                    name = instr.split(".")[1].split("@")[1];
+                }
+
+                this.vstack.set({
+                    name: name,
+                    value: this.stack.pop(),
+                    isConst: isConst
+                });
+
+                continue;
+            }
+
+            if (!/\s/g.test(instr) && /[a-zA-Z0-9-]/.test(instr)) {
                 this.logger.debug(`(${method}) Instruction found`);
 
                 if (instr in this.map) {
@@ -63,33 +90,46 @@ export class BoomBoom {
 
                     this.eval(instr);
                 } else {
-                    if (instr == "--boomboom-vstack-dump") {
-                        this.logger.debug(`(${method}) Dumping vstack`);
-                        this.vstack.dump();
-                    } else if (instr == "--boomboom-stack-dump") {
-                        this.logger.debug(`(${method}) Dumping stack`);
-                        this.stack.dump();
-                    } else
-                        throw new BBException(`Invalid method (${instr}) in ${method}`);
+                    // if (instr == "--boomboom-vstack-dump") {
+                    //     this.logger.debug(`(${method}) Dumping vstack`);
+                    //     this.vstack.dump();
+                    // } else if (instr == "--boomboom-stack-dump") {
+                    //     this.logger.debug(`(${method}) Dumping stack`);
+                    //     this.stack.dump();
+
+                    // } else
+                    //     throw new BBException(`Invalid method (${instr}) in ${method}`);
+                    switch (instr) {
+                        case "--boomboom-stack-dump":
+                            this.logger.debug(`(${method}) Dumping stack`);
+                            this.stack.dump();
+
+                            break;
+
+                        case "--boomboom-vstack-dump":
+                            this.logger.debug(`(${method}) Dumping vstack`);
+                            this.vstack.dump();
+
+                            break;
+
+                        case "puts":
+                            IOBuffer.putString(this.stack.pop());
+
+                            break;
+
+                        case "gets":
+                            this.stack.push(IOBuffer.getString(this.stack.pop()));
+
+                            break;
+
+                        default:
+                            throw new BBException(`Invalid instruction (${instr}) in ${method}`);
+
+                    }
                 }
 
                 continue;
             }
-
-            if (instr.startsWith(".")) {
-                this.logger.debug(`(${method}) Given expression is a pop-expression`);
-
-                if (!(!/\s/g.test(instr) && /^.?@[a-zA-Z0-9z][#]/.test(instr)))
-                    throw new BBException(`Malformed pop-expression (${instr}) in ${method}`);
-
-                this.vstack.set({
-                    name: instr.split(".")[1].split("@")[1],
-                    value: this.stack.pop(),
-                    isConst: false
-                });
-            }
-
-            throw new BBException(`Invalid instruction (${instr}) in ${method}`);
         }
 
         return;
